71a0112e3aef9038089b68305d387293ee1687a4
godL <547188371@qq.com>
2017-07-23 01:42:18 +0800

    initializeify project

---
+++ FHSchedulerDemo/FHSchedulerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FHSchedulerDemo
+//
+//  Created by 李浩 on 2017/7/23.
+//  Copyright © 2017年 GodL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FHSchedulerDemo/FHSchedulerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FHSchedulerDemo
+//
+//  Created by 李浩 on 2017/7/23.
+//  Copyright © 2017年 GodL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
